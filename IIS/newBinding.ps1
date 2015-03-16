84372bbdec6dd2db5e8e2200098049f9bd781c92
Dan Clegg <Dan@dclegg.byu.edu>
2015-03-16 13:58:41 -0600

    Adding webbinding script

---
+++ IIS/newBinding.ps1
@@ -0,0 +1,14 @@
+$param (
+	[string]$webApp = $(throw "-webApp is required. Use the name, not the web app url."),
+	[string]$Url = $(throw "-Url is required."),
+	[string]$protocol = "https",
+	[int]$Port = 443
+	[bool]$Ssl = $true
+)
+
+if (($Ssl -eq $true) -or ($protocol -eq "https")){ 
+New-WebBinding -Name "$($webApp)" -Force -HostHeader "hostHeaderSiteCollectionUrl" -Port "443" -Protocol "https" -sslFlags 1
+}
+else {
+New-WebBinding -Name "$($webApp)" -Force -HostHeader "$($Url) -Port $Port -Protocol $Protocol -sslFlags 0 
+}
